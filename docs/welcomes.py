bd7cad2de4300d80ee6d83bb98f8e2f3dc2fa94f
sjungmin98 <shc980@gmail.com>
2023-12-04 16:18:04 +0900

    ㅠㅠ

--- docs/welcomes.py
+++ docs/welcomes.py
@@ -1,7 +1,11 @@
 ## 변수 선언 후 정의 시 고려점 (넣는 값이 문자 or 숫자)
 # 문자 출력
+print("Hello, World..! Jungmin Seo") # 상수(왠만하면 사용X)
+
 helloworld = "Hello, World..! Jungmin Seo" #문자형 변수
 print(helloworld)
+
+
 
 # 숫자 합산 출력
 numbers = 3 + 5 #숫자형 변수
---
+++ onehandgame.py
@@ -0,0 +1,25 @@
+str_first = "1"
+str_second = ""
+str_third = "2"
+str_fourth = ""
+str_fifth = "3"
+result = "{} -> {} -> {} -> {} -> {}".format(str_first, str_second, str_third, str_fourth, str_fifth)
+print(result)
+result = "{} -> {} -> {} -> {} -> {}".format(str_second, str_first, str_third, str_fourth, str_fifth)
+print(result)
+result = "{} -> {} -> {} -> {} -> {}".format(str_fourth, str_fourth, str_third, str_first, str_fifth)
+print(result)
+result = "{} -> {} -> {} -> {} -> {}".format(str_fourth, str_third, str_fourth, str_first, str_fifth)
+print(result)
+result = "{} -> {} -> {} -> {} -> {}".format(str_fourth, str_third, str_fifth, str_first, str_second)
+print(result)
+result = "{} -> {} -> {} -> {} -> {}".format(str_fourth, str_third, str_fifth, str_second, str_first)
+print(result)
+result = "{} -> {} -> {} -> {} -> {}".format(str_fifth, str_third, str_second, str_second, str_first)
+print(result)
+result = "{} -> {} -> {} -> {} -> {}".format(str_fifth, str_second, str_third, str_second, str_first)
+print(result)
+result = "{} -> {} -> {} -> {} -> {}".format(str_first, str_second, str_third, str_fourth, str_fifth)
+print = (result)
+print = "ㅠㅠ"
+

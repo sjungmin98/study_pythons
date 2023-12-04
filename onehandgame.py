4049f9e18f4841752e8c4013e3e95a76900be3e2
sjungmin98 <shc980@gmail.com>
2023-12-04 16:22:25 +0900

    ohg1

--- onehandgame.py
+++ onehandgame.py
@@ -3,23 +3,32 @@ str_second = ""
 str_third = "2"
 str_fourth = ""
 str_fifth = "3"
-result = "{} -> {} -> {} -> {} -> {}".format(str_first, str_second, str_third, str_fourth, str_fifth)
-print(result)
-result = "{} -> {} -> {} -> {} -> {}".format(str_second, str_first, str_third, str_fourth, str_fifth)
-print(result)
-result = "{} -> {} -> {} -> {} -> {}".format(str_fourth, str_fourth, str_third, str_first, str_fifth)
-print(result)
-result = "{} -> {} -> {} -> {} -> {}".format(str_fourth, str_third, str_fourth, str_first, str_fifth)
-print(result)
-result = "{} -> {} -> {} -> {} -> {}".format(str_fourth, str_third, str_fifth, str_first, str_second)
-print(result)
-result = "{} -> {} -> {} -> {} -> {}".format(str_fourth, str_third, str_fifth, str_second, str_first)
-print(result)
-result = "{} -> {} -> {} -> {} -> {}".format(str_fifth, str_third, str_second, str_second, str_first)
-print(result)
-result = "{} -> {} -> {} -> {} -> {}".format(str_fifth, str_second, str_third, str_second, str_first)
-print(result)
-result = "{} -> {} -> {} -> {} -> {}".format(str_first, str_second, str_third, str_fourth, str_fifth)
-print = (result)
-print = "ㅠㅠ"
+print("{} {} {} {} {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
 
+str_second = str_first
+str_first = ""
+print("{} {} {} {} {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
+
+str_fourth = str_second
+str_second = ""
+print("{} {} {} {} {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
+
+str_second = str_third
+str_third = ""
+print("{} {} {} {} {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
+
+str_third = str_fifth
+str_fifth = ""
+print("{} {} {} {} {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
+
+str_fifth = str_fourth
+str_fourth = ""
+print("{} {} {} {} {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
+
+str_first = str_third 
+str_third = ""
+print("{} {} {} {} {}".format(str_first, str_second, str_third, str_fourth, str_fifth))
+
+str_third = str_second
+str_second = "" 
+print("{} {} {} {} {}".format(str_first, str_second, str_third, str_fourth, str_fifth))

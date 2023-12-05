f7281041de7ca576e9b304a0cba780f1c56617e6
sjungmin98 <shc980@gmail.com>
2023-12-05 16:52:09 +0900

    11

--- docs/quests/bmi.py
+++ docs/quests/bmi.py
@@ -4,10 +4,10 @@ weight = int(input("몸무게를 입력하세요. :"))
 BMI = weight / (height ** 2)
 
 if BMI >= 25 :
-    print("{}, 비만 입니다.".format(BMI))
+    print("당신의 BMI 수치는{}, 비만 입니다.".format(BMI))
 elif BMI >= 23 :
-    print("{}, 과체중 입니다.".format(BMI))
+    print("당신의 BMI 수치는{}, 과체중 입니다.".format(BMI))
 elif BMI >= 18 :
-    print("{}, 정상체중 입니다.".format(BMI))   
+    print("당신의 BMI 수치는{}, 정상체중 입니다.".format(BMI))   
 else :
-    print("{}, 저체중 입니다.".format(BMI))
+    print("당신의 BMI 수치는{}, 저체중 입니다.".format(BMI))

ea27fc201005771dd209abcb5cfab21a3d706929
sjungmin98 <shc980@gmail.com>
2023-12-05 12:28:10 +0900

    fff

--- docs/quests/imputwiththree_output.py
+++ docs/quests/imputwiththree_output.py
@@ -1,4 +1,9 @@
-a, b, c = input().split()
-x = "a*b*c"
+a1, b1, c1 = input().split()
 
-print("{}m * {}m * {}m = 직육면체 부피: {}m^3".format(int(a), int(b), int(c), x))
+a = int(a1)
+b = int(b1)
+c = int(c1)
+
+x = a*b*c
+
+print("{}m * {}m * {}m = 직육면체 부피: {}m^3".format(a, b, c, x))

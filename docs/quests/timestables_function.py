694c87b1d4b204c442f812febea98ab2b6c87156
sjungmin98 <shc980@gmail.com>
2023-12-08 10:46:47 +0900

    수정줄임

--- docs/quests/timestables_function.py
+++ docs/quests/timestables_function.py
@@ -1,32 +1,28 @@
-def multiply(number, multiplier):
-    return number * multiplier
+def multiplication(number):
+    for number_first in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        result = number * number_first
+        print("{} x {} = {}".format(number, number_first, result))
+
+def multiply_result(number, user_input):
+    result = number * int(user_input)
+    print("result for {}dan: {}".format(number, result))
 
 def cal_multi():
     numbers = [20, 30, 35]
 
     for number in numbers:
-        num = 1
-        while num <= 9:
-            result = multiply(number, num)
-            print("{} x {} = {}".format(number, num, result))
-            num += 1
+        multiplication(number)
+
+    print("-----------------------------")
+
+    while True:
+        for number in [20, 30, 35]:
+            user_input = input("{} * ".format(number))
+
+            if user_input == 'q':
+                print("Quit the calculator")
+                return
+
+            multiply_result(number, user_input)
 
 cal_multi()
-
-print("-----------------------------")
-
-def multiply_and_print(number):
-    multiplier = int(input("{} * ".format(number)))
-    if multiplier == 'q':
-        print("Quit the calculator")
-        return None
-    result = multiply(number, multiplier)
-    print("Result for {}: {}".format(number, result))
-
-def cal_multi_input():
-    numbers = [20, 30, 35]
-
-    for number in numbers:
-        multiply_and_print(number)
-
-cal_multi_input()

da2704cfbe9cd494548ab2efcbad205b04665c6f
sjungmin98 <shc980@gmail.com>
2023-12-08 10:20:53 +0900

    줄임

--- docs/quests/timestables_function.py
+++ docs/quests/timestables_function.py
@@ -1,64 +1,32 @@
-def cal_multi():
-    number_simple = 20
-    num = 1
-
-    while num <= 9 :
-        result = number_simple * num
-        print("{} x {} = {}".format(number_simple, num, result))
-        num += 1
-    
-    number_simple = 30
-    num = 1
-
-    while num <= 9 :
-        result = number_simple * num
-        print("{} x {} = {}".format(number_simple, num, result))
-        num += 1
-
-    number_simple = 35
-    num = 1
-    
-    while num <= 9 :
-        result = number_simple * num
-        print("{} x {} = {}".format(number_simple, num, result))
-        num += 1
-cal_multi()
-
-print ("-----------------------------")
-
-def multiply(num_first, num_second):
-    return num_first * num_second
-
-def multiply(num_third, num_fourth):
-    return num_third * num_fourth
-
-def multiply(num_fifth, num_sixth):
-    return num_fifth * num_sixth
+def multiply(number, multiplier):
+    return number * multiplier
 
 def cal_multi():
-    while True:
-        num_first = 20
-        num_second = input("20 * ")
-        num_third = 30
-        num_fourth = input("30 * ")
-        num_fifth = 35
-        num_sixth = input("35 * ")
+    numbers = [20, 30, 35]
 
-        if num_second == 'q' or num_fourth == 'q' or num_sixth == 'q':
-            print("Quit the calculator")
-            break
-        
-        num_second = int(num_second)
-        num_fourth = int(num_fourth)
-        num_sixth = int(num_sixth)
-
-        result = multiply(num_first, num_second)
-        result_30 = multiply(num_third, num_fourth)
-        result_35 = multiply(num_fifth, num_sixth)
-
-        print("result for 20dan: {}".format(result))
-        print("result for 30dan: {}".format(result_30))
-        print("result for 35dan: {}".format(result_35))
+    for number in numbers:
+        num = 1
+        while num <= 9:
+            result = multiply(number, num)
+            print("{} x {} = {}".format(number, num, result))
+            num += 1
 
 cal_multi()
 
+print("-----------------------------")
+
+def multiply_and_print(number):
+    multiplier = int(input("{} * ".format(number)))
+    if multiplier == 'q':
+        print("Quit the calculator")
+        return None
+    result = multiply(number, multiplier)
+    print("Result for {}: {}".format(number, result))
+
+def cal_multi_input():
+    numbers = [20, 30, 35]
+
+    for number in numbers:
+        multiply_and_print(number)
+
+cal_multi_input()

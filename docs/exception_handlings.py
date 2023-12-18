5eaf3f68ff61b6fd82b37d68d47072a1aa2b0b4d
sjungmin98 <shc980@gmail.com>
2023-12-19 08:55:14 +0900

    11

---
+++ docs/exception_handlings.py
@@ -0,0 +1,26 @@
+# 기본 구문
+try : 
+     pass   # 업무 코드
+except:
+     pass   # 업무 코드 문제 발생 시 대처 코드
+finally :
+     pass   # try나 excpet이 끝난 후 무조건 실행 코드
+
+# pure python with calculator
+
+num_first = 4
+num_second = 5
+# 곱셉 연산
+
+# 기본 구문
+try : 
+    result = num_first / num_second 
+    pass   # 업무 코드
+except:
+    result = int(num_first) / int(num_second)
+    pass   # 업무 코드 문제 발생 시 대처 코드
+finally :
+    pass   # try나 excpet이 끝난 후 무조건 실행 코드
+
+print("{} = {} / {}".format(result, num_first, num_second))
+pass
